dca68fa8728a0086e599280d2ac1a6ee1107a4db
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2020-01-23 06:10:43 +0000

    Show cube size in window title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,12 +6,31 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Cube *c = new Cube();
-    ui->graphicsView->initialize(c);
+
+    //create the cube and give it to the CubeGraphicsView
+    cube = new Cube();
+    ui->graphicsView->initialize(cube);
+
+    //set the window title
+    updateTitleText();
+
+    //detect when the cube size changes so we can update the window title
+    connect(cube, SIGNAL(cubeSizeChanged()), this, SLOT(onCubeSizeChanged()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateTitleText(){
+    QString size = QString::number(cube->getSize());
+    QString str = "cubesim (" + size + "x" + size + "x" + size + ")";
+
+    setWindowTitle(str);
+}
+
+void MainWindow::onCubeSizeChanged(){
+    updateTitleText();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "cube.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,13 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    Cube *cube;
+
+    void updateTitleText();
+
+private slots:
+    void onCubeSizeChanged();
+
 };
 #endif // MAINWINDOW_H
