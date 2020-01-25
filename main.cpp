c49dc8f187cfca20b2bf2053e86c0df07283fae1
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2020-01-25 11:09:29 +0000

    Seeded the random number generator

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDateTime>
 
 int main(int argc, char *argv[])
 {
+    qsrand(QDateTime::currentMSecsSinceEpoch());
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
